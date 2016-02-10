4ec2737e60a7446bc0c06f3693439bef46e75668
glacier <glacier@yoshidahyoumiyabi-no-MacBook-Air.local>
2016-02-10 20:58:03 +0900

    feader, footerのとりあえず完成

--- public/script.js
+++ public/script.js
@@ -1,10 +1,23 @@
 $(function() {
 	$(window).on('scroll', function() {
-		if ($(document).scrollTop() > 100) {
+		if ($(document).scrollTop() > 135) {
 			$('.fixbar').addClass('fixed');
 		} else {
 			$('.fixbar').removeClass('fixed');
 		}
 	});
 });
+
+$(function() {
+	$(".link").click(function(e){
+		e.preventDefault();
+		var target = $(this).attr("href");
+		var offset = $(target).offset().top;
+		
+		$("body, html").animate({
+			scrollTop: offset - 100 
+		}, 1000);
+	});
+});
+
 
