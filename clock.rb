2aa24aab506d7408dcf3c4f3adb813475c0d7093
g-hyoga <hyoga0216@gmail.com>
2016-03-10 16:52:18 +0900

    #3 治った？ #4 も実装してた

--- clock.rb
+++ clock.rb
@@ -8,15 +8,17 @@ include Clockwork
 
 every(30.minutes, 'work')  do
 	#urlとsearch_word, mail adressをmysqlからとってくる
-	client= Mysql.connect('localhost', 'root', MYSQL_PASS, 'notice')
+	client = Mysql.connect('localhost', 'root', MYSQL_PASS, 'notice')
 	client.query("select noticed from sites").each do |noticed|
 		if noticed[0].to_i == 0 then
 			client.query("select url, keyword, email from sites").each do |url, word, mail_adress|
+				puts "seeking in " + url, word, mail_adress
 				#通知
 				notice(url, word, mail_adress)
 				#noticed をtrueにする
-				stmt = client.prepare("update sites set noticed = 1 where url = ? && keyword = ? && email = ?")
-				stmt.execute url, word, mail_adress
+				#stmt = client.prepare("update sites set noticed = 1 where url = ? && keyword = ? && email = ?")
+				#stmt.execute url, word, mail_adress
+				puts " "
 			end
 		end
 	end
